c89f1151575f012d0f4d94f5309e5269a8877f6f
NickArch94 <158612778+NickArch94@users.noreply.github.com>
2025-06-23 11:57:36 -0600

    Followed the instructions in 'ReactSpinUp' and have React.JS set up with the initial files + dark mode.

---
+++ frontend/app/routes/home.tsx
@@ -0,0 +1,18 @@
+import type { Route } from "./+types/home";
+
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return(
+      <>
+        <h1 className={'text-3xl font-bold'}> Home </h1>
+      </>
+  )
+
+}
