f147d2a62084890ffb793b89e7c02e5b100894ec
Kilian Rempen <kilianrempen@gmail.com>
2025-06-27 09:01:59 -0600

    added hihglighting for current page

--- frontend/src/layouts/navigation.tsx
+++ frontend/src/layouts/navigation.tsx
@@ -1,9 +1,14 @@
 import {MdOutlineMessage} from "react-icons/md";
 import {PiPlugsConnectedFill} from "react-icons/pi";
 import {CgProfile} from "react-icons/cg";
+import {useEffect} from "react";
 
 
 export function Navigation() {
+    useEffect(() => {
+        currentPageNav()
+    }, [])
+
     return (
         <>
             <nav>
@@ -14,24 +19,47 @@ export function Navigation() {
                         <h2 className="self-center whitespace-nowrap tracking-tight text-3xl font-semibold text-gray-800">Commonality</h2>
                     </div>
                     <div className="md:flex hidden justify-center items-center gap-10 min-h-8">
-                        <a className="flex items-center gap-2  text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" href={"/messages"}><MdOutlineMessage />Messages</a> |
-                        <a className="flex items-center gap-2  text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" href="/connect"><PiPlugsConnectedFill />Make Connections</a> |
-                        <a className="flex items-center gap-2 text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" href="/profile"><CgProfile />Profile</a>
+                        <a id="desktop-messages-link" className="flex items-center gap-2 text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" href={"/messages"}><MdOutlineMessage />Messages</a> |
+                        <a id="desktop-connect-link" className="flex items-center gap-2 text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" href="/connect"><PiPlugsConnectedFill />Make Connections</a> |
+                        <a id="desktop-profile-link" className="flex items-center gap-2 text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" href="/profile"><CgProfile />Profile</a>
                     </div>
                 </div>
                 {/*MOBILE HEADER with FOOTER*/}
                 <div className="md:hidden fixed bottom-0 w-full bg-white border-t shadow-inner z-50 text-gray-700">
                     <div className="md:hidden flex justify-evenly items-center gap-3 px-2 py-2 min-h-20">
-                        <a className="flex flex-col items-center gap-2  text-md hover:text-blue-600 hover:border-b-2" href={"/messages"}><MdOutlineMessage />Messages</a> |
-                        <a className="flex flex-col items-center gap-2  text-md hover:text-blue-600 hover:border-b-2" href="/connect"><PiPlugsConnectedFill />Make Connections</a> |
-                        <a className="flex flex-col items-center gap-2 text-md hover:text-blue-600 hover:border-b-2" href="/profile"><CgProfile />Profile</a>
+                        <a id="mobile-messages-link" className="flex flex-col items-center gap-2 text-md hover:text-blue-600 hover:border-b-2" href={"/messages"}><MdOutlineMessage />Messages</a> |
+                        <a id="mobile-connect-link" className="flex flex-col items-center gap-2 text-md hover:text-blue-600 hover:border-b-2" href="/connect"><PiPlugsConnectedFill />Make Connections</a> |
+                        <a id="mobile-profile-link" className="flex flex-col items-center gap-2 text-md hover:text-blue-600 hover:border-b-2" href="/profile"><CgProfile />Profile</a>
                     </div>
                     <div className={" bg-black text-gray-200 py-2 text-center italic text-xs"}>
                         <p>Copyright 2025 Commonality</p>
                     </div>
                 </div>
+                <button onClick = {currentPageNav}>TEST BUTTON</button>
             </nav>
         </>
     )
 }
 
+
+
+function currentPageNav() {
+    const loc = document.location
+    const desktopMessagesLink = document.getElementById('desktop-messages-link');
+    const mobileMessagesLink = document.getElementById('mobile-messages-link');
+    const desktopConnectLink = document.getElementById('desktop-connect-link');
+    const mobileConnectLink = document.getElementById('mobile-connect-link');
+    const desktopProfileLink = document.getElementById('desktop-profile-link');
+    const mobileProfileLink = document.getElementById('mobile-profile-link');
+    if (loc.pathname === '/messages') {
+        desktopMessagesLink.className = "flex items-center gap-2 text-lg text-blue-700 border-b-2 border-blue-700";
+        mobileMessagesLink.className = "flex flex-col items-center gap-2 text-md text-blue-600 border-b-2";
+    } if (loc.pathname === '/connect') {
+        desktopConnectLink.className = "flex items-center gap-2 text-lg text-blue-700 border-b-2 border-blue-700";
+        mobileConnectLink.className = "flex flex-col items-center gap-2 text-md text-blue-600 border-b-2";
+    } if (loc.pathname === '/profile') {
+        desktopProfileLink.className = "flex items-center gap-2 text-lg text-blue-700 border-b-2 border-blue-700";
+        mobileProfileLink.className = "flex flex-col items-center gap-2 text-md text-blue-600 border-b-2";
+    }
+    console.log(document.location)
+}
