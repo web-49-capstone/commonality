a2a5867e0990d6847fcb74adb227ac2827b3ceec
Kilian Rempen <kilianrempen@gmail.com>
2025-06-27 09:31:12 -0600

    changing href to to

--- frontend/src/layouts/navigation.tsx
+++ frontend/src/layouts/navigation.tsx
@@ -20,17 +20,17 @@ export function Navigation() {
                         <h2 className="self-center whitespace-nowrap tracking-tight text-3xl font-semibold text-gray-800">Commonality</h2>
                     </div>
                     <div className="md:flex hidden justify-center items-center gap-10 min-h-8">
-                        <Link id="desktop-messages-link" className="flex items-center gap-2 text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" href="/messages"><MdOutlineMessage />Messages</Link> |
-                        <Link id="desktop-connect-link" className="flex items-center gap-2 text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" href="/connect"><PiPlugsConnectedFill />Make Connections</Link> |
-                        <Link id="desktop-profile-link" className="flex items-center gap-2 text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" href="/profile"><CgProfile />Profile</Link>
+                        <Link id="desktop-messages-link" className="flex items-center gap-2 text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" to="/messages"><MdOutlineMessage />Messages</Link> |
+                        <Link id="desktop-connect-link" className="flex items-center gap-2 text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" to="/connect"><PiPlugsConnectedFill />Make Connections</Link> |
+                        <Link id="desktop-profile-link" className="flex items-center gap-2 text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" to="/profile"><CgProfile />Profile</Link>
                     </div>
                 </div>
                 {/*MOBILE HEADER with FOOTER*/}
                 <div className="md:hidden fixed bottom-0 w-full bg-white border-t shadow-inner z-50 text-gray-700">
                     <div className="md:hidden flex justify-evenly items-center gap-3 px-2 py-2 min-h-20">
-                        <Link id="mobile-messages-link" className="flex flex-col items-center gap-2 text-md hover:text-blue-600 hover:border-b-2" href={"/messages"}><MdOutlineMessage />Messages</Link> |
-                        <Link id="mobile-connect-link" className="flex flex-col items-center gap-2 text-md hover:text-blue-600 hover:border-b-2" href="/connect"><PiPlugsConnectedFill />Make Connections</Link> |
-                        <Link id="mobile-profile-link" className="flex flex-col items-center gap-2 text-md hover:text-blue-600 hover:border-b-2" href="/profile"><CgProfile />Profile</Link>
+                        <Link id="mobile-messages-link" className="flex flex-col items-center gap-2 text-md hover:text-blue-600 hover:border-b-2" to={"/messages"}><MdOutlineMessage />Messages</Link> |
+                        <Link id="mobile-connect-link" className="flex flex-col items-center gap-2 text-md hover:text-blue-600 hover:border-b-2" to="/connect"><PiPlugsConnectedFill />Make Connections</Link> |
+                        <Link id="mobile-profile-link" className="flex flex-col items-center gap-2 text-md hover:text-blue-600 hover:border-b-2" to="/profile"><CgProfile />Profile</Link>
                     </div>
                     <div className={" bg-black text-gray-200 py-2 text-center italic text-xs"}>
                         <p>Copyright 2025 Commonality</p>
