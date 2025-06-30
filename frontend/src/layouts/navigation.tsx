9f6d0a46f2c973656ce7e6462cf7d452ffa86c62
Kilian Rempen <kilianrempen@gmail.com>
2025-06-29 22:18:13 -0600

    nav bar navlink classname setup

--- frontend/src/layouts/navigation.tsx
+++ frontend/src/layouts/navigation.tsx
@@ -1,14 +1,27 @@
 import {MdOutlineMessage} from "react-icons/md";
 import {PiPlugsConnectedFill} from "react-icons/pi";
 import {CgProfile} from "react-icons/cg";
-import {useEffect} from "react";
-import {Link} from "react-router";
+import React, {useEffect, useState} from "react";
+import {NavLink} from "react-router";
+import {useRef} from "react";
+import {useLocation} from "react-router"
 
 
-export function Navigation() {
-    useEffect(() => {
-        currentPageNav()
-    }, [])
+export function Navigation(){
+    const loc = useLocation()
+
+    const [currentPage, setCurrentPage] = useState(loc.pathname)
+
+    const desktopMessagesRef = useRef(null)
+    const desktopConnectRef = useRef(null)
+    const desktopProfileRef = useRef(null)
+    const mobileMessagesRef = useRef(null)
+    const mobileConnectRef = useRef(null)
+    const mobileProfileRef = useRef(null)
+    const desktopNavClasses = "flex items-center gap-2 text-lg text-gray-700 hover:text-blue-700 hover:border-b-2"
+    const mobileNavClasses = "flex flex-col items-center gap-2 text-md hover:text-blue-600 hover:border-b-2"
+    const desktopCurrentNavClasses = "flex items-center gap-2 text-lg text-blue-700 border-b-2 border-blue-700"
+    const mobileCurrentNavClasses = "flex flex-col items-center gap-2 text-md text-blue-600 border-b-2"
 
     return (
         <>
@@ -20,47 +33,26 @@ export function Navigation() {
                         <h2 className="self-center whitespace-nowrap tracking-tight text-3xl font-semibold text-gray-800">Commonality</h2>
                     </div>
                     <div className="md:flex hidden justify-center items-center gap-10 min-h-8">
-                        <Link id="desktop-messages-link" className="flex items-center gap-2 text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" to="/messages"><MdOutlineMessage />Messages</Link> |
-                        <Link id="desktop-connect-link" className="flex items-center gap-2 text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" to="/connect"><PiPlugsConnectedFill />Make Connections</Link> |
-                        <Link id="desktop-profile-link" className="flex items-center gap-2 text-lg text-gray-700 hover:text-blue-700 hover:border-b-2" to="/profile"><CgProfile />Profile</Link>
+                        <NavLink to="/messages" className={({isActive, isPending}) => isActive ? desktopCurrentNavClasses : desktopNavClasses}><MdOutlineMessage />Messages</NavLink> |
+                        <NavLink to="/connect" className={({isActive, isPending}) => isActive ? desktopCurrentNavClasses : desktopNavClasses}><PiPlugsConnectedFill />Make Connections</NavLink> |
+                        <NavLink to="/profile"><span className={desktopNavClasses}><CgProfile />Profile</span></NavLink>
                     </div>
+
                 </div>
                 {/*MOBILE HEADER with FOOTER*/}
                 <div className="md:hidden fixed bottom-0 w-full bg-white border-t shadow-inner z-50 text-gray-700">
                     <div className="md:hidden flex justify-evenly items-center gap-3 px-2 py-2 min-h-20">
-                        <Link id="mobile-messages-link" className="flex flex-col items-center gap-2 text-md hover:text-blue-600 hover:border-b-2" to={"/messages"}><MdOutlineMessage />Messages</Link> |
-                        <Link id="mobile-connect-link" className="flex flex-col items-center gap-2 text-md hover:text-blue-600 hover:border-b-2" to="/connect"><PiPlugsConnectedFill />Make Connections</Link> |
-                        <Link id="mobile-profile-link" className="flex flex-col items-center gap-2 text-md hover:text-blue-600 hover:border-b-2" to="/profile"><CgProfile />Profile</Link>
+                        <NavLink to="/messages" className={({isActive, isPending}) => isActive ? mobileCurrentNavClasses : mobileNavClasses}><MdOutlineMessage />Messages</NavLink> |
+                        <NavLink to="/connect" className={({isActive, isPending}) => isActive ? mobileCurrentNavClasses : mobileNavClasses}><PiPlugsConnectedFill />Make Connections</NavLink> |
+                        <NavLink to="/profile" className={({isActive, isPending}) => isActive ? mobileCurrentNavClasses : mobileNavClasses}><CgProfile />Profile</NavLink>
                     </div>
-                    <div className={" bg-black text-gray-200 py-2 text-center italic text-xs"}>
+                    <div className={"bg-black text-gray-200 py-2 text-center italic text-xs"}>
                         <p>Copyright 2025 Commonality</p>
                     </div>
                 </div>
-                <button onClick = {currentPageNav}>TEST BUTTON</button>
             </nav>
         </>
     )
+
 }
 
-
-
-function currentPageNav() {
-    const loc = document.location
-    const desktopMessagesLink = document.getElementById('desktop-messages-link');
-    const mobileMessagesLink = document.getElementById('mobile-messages-link');
-    const desktopConnectLink = document.getElementById('desktop-connect-link');
-    const mobileConnectLink = document.getElementById('mobile-connect-link');
-    const desktopProfileLink = document.getElementById('desktop-profile-link');
-    const mobileProfileLink = document.getElementById('mobile-profile-link');
-    if (loc.pathname === '/messages') {
-        desktopMessagesLink.className = "flex items-center gap-2 text-lg text-blue-700 border-b-2 border-blue-700";
-        mobileMessagesLink.className = "flex flex-col items-center gap-2 text-md text-blue-600 border-b-2";
-    } if (loc.pathname === '/connect') {
-        desktopConnectLink.className = "flex items-center gap-2 text-lg text-blue-700 border-b-2 border-blue-700";
-        mobileConnectLink.className = "flex flex-col items-center gap-2 text-md text-blue-600 border-b-2";
-    } if (loc.pathname === '/profile') {
-        desktopProfileLink.className = "flex items-center gap-2 text-lg text-blue-700 border-b-2 border-blue-700";
-        mobileProfileLink.className = "flex flex-col items-center gap-2 text-md text-blue-600 border-b-2";
-    }
-    console.log(document.location)
-}
