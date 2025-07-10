69ce081bb43c7b2c97d7f6e30825717001a4bf9f
NickArch94 <158612778+NickArch94@users.noreply.github.com>
2025-07-10 17:01:29 -0600

    Created a 'matches' branch, and am working on the matching backend components for that specific feature.

---
+++ backend/src/apis/matching/matching.controller.ts
@@ -0,0 +1,36 @@
+import {
+    MatchSchema,
+    type Match,
+    selectMatchByMatchMakerId,
+    selectMatchByMatchReceiverId,
+    insertMatch, selectAllMatches
+} from "./matching.model.ts"
+import {serverErrorResponse, zodErrorResponse} from "../../utils/response.utils.ts";
+import type {Request, Response} from "express"
+import type {Status as HttpStatus} from "../../utils/interfaces/Status.ts"
+
+export async function getMatchesByMatchMakerIdController (request: Request, response: Response): Promise<void> {
+    try {
+        const data = await selectAllMatches()
+        const httpStatus = {status: 200, message: null, data}
+        response.json (httpStatus)
+    } catch (error) {
+        console.error(error)
+        serverErrorResponse(response, [])
+    }
+}
+
+export async function getMatchesByMatchReceiverId (request: Request, response: Response): Promise<void> {
+    try {
+        const validationResult = MatchSchema
+            .pick({matchId: true})
+            .safeParse(request.params)
+    } if (!validationResult.success) {
+        zodErrorResponse(response, validationResult.error)
+        return
+    }
+
+    const
+}
+
+
