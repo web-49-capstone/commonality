a9f39e1716835ad718819913dc430c4097db71c7
Kilian Rempen <kilianrempen@gmail.com>
2025-07-14 11:07:51 -0600

    made matchAccepted nullable in Zod

--- backend/src/apis/matching/matching.model.ts
+++ backend/src/apis/matching/matching.model.ts
@@ -4,7 +4,7 @@ import { sql } from "../../utils/database.utils.ts"
 export const MatchSchema = z.object({
     matchMakerId: z.uuidv7('please provide a valid uuid7 for matchMakerId'),
     matchReceiverId: z.uuidv7('please provide a valid uuid7 for matchRecieverId'),
-    matchAccepted: z.boolean('Please provide a valid matchAccepted boolean'),
+    matchAccepted: z.boolean('Please provide a valid matchAccepted boolean').nullable(),
     matchCreated: z.coerce.date('Please provide a valid matchCreated date')
     .nullable()
 })
