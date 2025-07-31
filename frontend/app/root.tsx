49d7d0e347c84fb86b9b3b564b8c7275f5f2ee50
Dylan Keck <dkeck@cnm.edu>
2025-07-31 15:33:36 -0600

    added cancel button on edit profile

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -41,7 +41,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <ScrollRestoration />
         <Scripts />
-      <Footer />
       </body>
     </html>
   );
--- frontend/app/routes/my-profile.tsx
+++ frontend/app/routes/my-profile.tsx
@@ -50,6 +50,16 @@ export default function MyProfile({loaderData}: Route.ComponentProps) {
                 </div>
 
                 {editMode ? (
+                    <div>
+                    <div className="flex justify-end mb-4 mr-9">
+                        <button
+                            onClick={() => setEditMode(false)}
+                            className="bg-red-600 text-gray-800 px-4 py-2 rounded-lg shadow hover:bg-gray-400 transition align-right text-white"
+                        >
+                            Cancel
+                        </button>
+                    </div>
+
                     <EditProfile
                         user={initialUser}
                         interests={interests}
@@ -57,6 +67,8 @@ export default function MyProfile({loaderData}: Route.ComponentProps) {
                         q={q}
                         userInterests={userInterests}
                     />
+</div>
+
                 ) : (
                     <div className="space-y-4 bg-white shadow-md p-6 rounded-xl">
                         <div className="flex items-center gap-4">
